2383a60ce2a992903a3269687ec762671d506685
Alex Simpson <alexsimpson440dev@gmail.com>
2019-03-30 16:02:09 -0500

    More structure but css and html not linking up.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 
 // create server
 http.createServer(function (req, res) {
-    fs.readFile('../public/html/index.html', function(err, data) {
+    fs.readFile('./public/html/index.html', function(err, data) {
         res.writeHead(200, {'Content-Type' : 'text/html'});
         res.write(data);
         res.end();
