5a6de3b4043d8d8549dc615ab31af4388808a29f
Alex Simpson <alexsimpson440dev@gmail.com>
2019-04-16 19:59:53 -0500

    added routes and set them up in the server.

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const path = require('path');
-
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+//routes
+var indexRouter = require('./routes/index');
+var beerRouter = require('./routes/beer');
+
 // static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+//something
+app.use('/', indexRouter);
+app.use('/beer.html', beerRouter);
 
 app.listen(PORT, () => 
     console.log(`Server started on port ${PORT}`)
