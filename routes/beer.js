0ad670d0b4d7c874f945b3a0e8937d365b049297
Alex Simpson <alexsimpson440dev@gmail.com>
2019-04-28 18:05:18 -0500

    Added basic json data and a couple links on the index/beer page.

--- routes/beer.js
+++ routes/beer.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
+const data = require('../beer.json')
+
+var ipa = data['beer'][0]
 
 router.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/views/beer.html'));
+    res.render('beer', {title: 'Home', message: 'Welcome the Beer Page'});
+})
+// todo: Put a for loop here to look through all json data and make a route/page for it.
+router.get('/ipa', function (req, res) {
+    res.render('beer', {title: ipa['type'], message: `The ${ipa['type']} has a ${ipa['flavor']} flavor.`})
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -3,20 +3,21 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-//routes
-var indexRouter = require('./routes/index');
-var beerRouter = require('./routes/beer');
-
 // static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-//something
+// setting the view location & engine
 app.set('views', './public/views')
 app.set('view engine', 'pug')
 
-app.use('/', indexRouter);
-app.use('/beer.html', beerRouter);
+// routes & express calls
+var indexRouter = require('./routes/index');
+var beerRouter = require('./routes/beer');
 
+app.use('/', indexRouter);
+app.use('/beer', beerRouter);
+
+// call server
 app.listen(PORT, () => 
     console.log(`Server started on port ${PORT}`)
 );
