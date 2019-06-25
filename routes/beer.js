c70f435f307d985102e1ff4a4dd3df948f2b83a5
Alex Simpson <alexsimpson440dev@gmail.com>
2019-06-25 14:30:40 -0500

    Updated routing. Still think there could be improvements though as in refreshing the page, error handling, and so on

--- routes/beer.js
+++ routes/beer.js
@@ -10,16 +10,27 @@ router.get('/', function (req, res) {
     res.render('beer', {title: 'Home', message: 'Welcome the Beer Page', data: beer});
 })
 
-router.get('/ipa', function (req, res) {
-    res.render('beer', {title: `${beer[0]['type']}`, message: `The ${beer[0]['type']} has a ${beer[0]['flavor']} flavor`, data: beer})
-})
+router.get(`/*`, function (req, res) {
+    let url = req.url.replace('/','')
 
-router.get('/stout', function (req, res) {
-    res.render('beer', {title: `${beer[1]['type']}`, message: `The ${beer[1]['type']} has a ${beer[1]['flavor']} flavor`, data: beer})
+    for (let i = 0; i < beer.length; i++) {
+        if (beer[i]['type'] == url) {
+            res.render('beer', {title: `${beer[i]['type']}`, message: `The ${beer[i]['type']} has a ${beer[i]['flavor']} flavor`, data: beer})
+        }
+    }
+    
+    res.render('error', {title: 'error', message: 'An error occurred attempting to find the page!'})
 })
+// router.get('/ipa', function (req, res) {
+//     res.render('beer', {title: `${beer[0]['type']}`, message: `The ${beer[0]['type']} has a ${beer[0]['flavor']} flavor`, data: beer})
+// })
 
-router.get('/sour', function (req, res) {
-    res.render('beer', {title: `${beer[2]['type']}`, message: `The ${beer[2]['type']} has a ${beer[2]['flavor']} flavor`, data: beer})
-})
+// router.get('/stout', function (req, res) {
+//     res.render('beer', {title: `${beer[1]['type']}`, message: `The ${beer[1]['type']} has a ${beer[1]['flavor']} flavor`, data: beer})
+// })
+
+// router.get('/sour', function (req, res) {
+//     res.render('beer', {title: `${beer[2]['type']}`, message: `The ${beer[2]['type']} has a ${beer[2]['flavor']} flavor`, data: beer})
+// })
 
 module.exports = router
